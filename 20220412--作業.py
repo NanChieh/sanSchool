9c350cba19da47d51ed5030d1f46fd6eadb75e78
NanChieh <106000021+NanChieh@users.noreply.github.com>
2022-05-28 15:33:07 +0800

    Add files via upload

---
+++ 20220412--作業.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 28 15:16:11 2022
+
+@author: NanChiehHsu
+"""
+
+print('第一個作業：')
+for a in range(1,6):
+    for b in range(a):
+        print(a,end='')
+    print()
+
+print('第二個作業：')
+for a in range(5,0,-1):
+    for b in range(a):
+        print(a,end='')
+    print()
+
+print('第三個作業：')
+for a in range(9,0,-2):
+    for b in range(a):
+        print(a,end='')
+    print()
+    
